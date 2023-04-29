98d33f34665e8ca4330fb24cff7d063db60fc152
fono09 <fono@fono.jp>
2023-04-29 19:56:03 +0900

    レプリカ実験成功

--- replica/entrypoint.sh
+++ replica/entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-while ! psql -h primary -U $POSTGRES_USER -d $POSTGRES_DB -p 5432 -c "select 'it is running';" 2>&1 ; do \
+while ! psql -U postgres -h 192.168.100.101 -c "select 'it is running';" 2>&1 ; do \
 	sleep 1s ; \
 done
 
 # load backup from primary instance
-pg_basebackup -h primary -p 5432 -D $PGDATA -S replication_slot_slave1 --progress -X stream -U replicator -Fp -R || :
+pg_basebackup -h 192.168.100.101 -D $PGDATA -S replication_slot_slave1 --progress -X stream -U replicator -Fp -R || :
 
 # start postgres
 bash /usr/local/bin/docker-entrypoint.sh -c 'config_file=/etc/postgresql/postgresql.conf' -c 'hba_file=/etc/postgresql/pg_hba.conf'
